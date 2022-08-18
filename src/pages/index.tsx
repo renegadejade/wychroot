bdb88fe96fbfd197b8e339afd3e399ec713963bc
John Thomas <jt@johnthomas.tech>
2022-08-18 18:05:39 -0400

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import RollDice from "../components/Dice/RollDice";
 
 import styles from "./index.module.css";
 
@@ -19,6 +20,7 @@ function HomepageHeader() {
             Wychroot Intro - 5min ⏱️
           </Link>
         </div>
+        <RollDice />
       </div>
     </header>
   );
